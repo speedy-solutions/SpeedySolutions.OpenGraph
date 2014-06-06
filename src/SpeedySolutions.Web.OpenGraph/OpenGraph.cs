9ffa2707c3e2159e9798f3af48ed4dff1754b29a
Vinicius Melquiades <vinicius.melquiades@outlook.com>
2014-06-05 22:18:19 -0300

    The OpenGraph now implements the IHtmlString interface, so it can be written directly to a Razor @ directive. I also refactored the code that generate the html tags

--- src/SpeedySolutions.Web.OpenGraph/OpenGraph.cs
+++ src/SpeedySolutions.Web.OpenGraph/OpenGraph.cs
@@ -2,22 +2,31 @@
 using System.Text;
 using System.Reflection;
 using System.Web;
+using System.Linq;
 
 namespace SpeedySolutions.Web.OpenGraph
 {
-	public class OpenGraph(string title = null, Uri url = null, OgImage image = null, string type = "website")
+	public class OpenGraph(string title = null, Uri url = null, OgImage image = null, string type = "website") : IHtmlString
 	{
 		[OgProperty("title", required: true)]
-		public string Title { get; set; } = title;
+		public string Title
+		{ get; set; }
+		= title;
 
 		[OgProperty("type", required: true)]
-		public string Type { get; } = type;
+		public string Type
+		{ get; }
+		= type;
 
 		[OgProperty("url", required: true)]
-		public Uri Url { get; set; } = url;
+		public Uri Url
+		{ get; set; }
+		= url;
 
 		[OgProperty("image", required: true)]
-		public OgImage Image { get; set; } = image;
+		public OgImage Image
+		{ get; set; }
+		= image;
 
 		public override string ToString()
 		{
@@ -28,38 +37,41 @@ namespace SpeedySolutions.Web.OpenGraph
 			return sb.ToString();
 		}
 
-		public IHtmlString ToHtmlString()
-		{
-			return new HtmlString(ToString());
-		}
-
 		private static void GetTags(object obj, StringBuilder sb, string prefix = null)
 		{
 			var properties = obj.GetType().GetRuntimeProperties();
 
-			foreach (var prop in properties)
+			foreach(var prop in properties)
 			{
-				foreach (var attr in prop.GetCustomAttributes<OgPropertyAttribute>())
+				foreach(var attr in prop.GetCustomAttributes<OgPropertyAttribute>())
 				{
-					var value = prop.GetValue(obj);
-					var ogProperty = attr.ToString(prefix);
-					if (value == null || value is string && string.IsNullOrWhiteSpace((string)value))
-					{
-						if (attr.Required)
-							throw new ArgumentNullException("value", string.Format("The property {0} is required", attr.ToString()));
-						else
-							continue;
-					}
-					else if (value is string || value is Uri || value is int)
-					{
-						sb.AppendLine(string.Format(@"<meta property=""{0}"" content=""{1}"">", ogProperty, value));
-					}
-					else
-					{
-						GetTags(value, sb, ogProperty);
-					}
+					BuildTagForAttribute(obj, sb, prefix, prop, attr);
 				}
 			}
+		}
+
+		private static void BuildTagForAttribute(object obj, StringBuilder sb, string prefix, PropertyInfo prop, OgPropertyAttribute attr)
+		{
+			var value = prop.GetValue(obj);
+			var ogProperty = attr.ToString(prefix);
+			var isString = value is string;
+			if(attr.Required && (value == null || isString && string.IsNullOrWhiteSpace((string)value)))
+			{
+				throw new ArgumentNullException("value", string.Format("The property {0} is required", attr.ToString()));
+			}
+			else if(isString || value is Uri || value is int)
+			{
+				sb.AppendLine(string.Format(@"<meta property=""{0}"" content=""{1}"">", ogProperty, value));
+			}
+			else
+			{
+				GetTags(value, sb, ogProperty);
+			}
+		}
+
+		public string ToHtmlString()
+		{
+			return ToString();
 		}
 	}
 }

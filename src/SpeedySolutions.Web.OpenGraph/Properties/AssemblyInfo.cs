1de34f82e85d120f31996f6999c8af4c8beba3d2
Vinicius Melquiades <vinicius.melquiades@outlook.com>
2014-06-07 23:14:31 -0300

    - Updated the AssemblyInfo.cs file with information about the package.   - Updated the nuspec file to read information from the included assembly.

--- src/SpeedySolutions.Web.OpenGraph/Properties/AssemblyInfo.cs
+++ src/SpeedySolutions.Web.OpenGraph/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpeedySolutions.OpenGraph")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple .NET helper to generate Open Graph meta tags")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Speedy Solutions")]
 [assembly: AssemblyProduct("SpeedySolutions.OpenGraph")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
